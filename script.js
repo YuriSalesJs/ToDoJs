d5edcc9a1a416cba978acb3ac7ce317d93c44875
Yuri <yurisales2000@hotmail.com>
2025-01-15 15:51:12 -0300

    função inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let tarefa = document.getElementById('tarefa_Input')
+let listaTarefas = document.getElementById('task_list')
+
+function adicionar (){
+    if(tarefa.value == ''){
+        window.alert('[Erro] adicionar uma tarefa.')
+    }else {
+        listaTarefas.insertAdjacentHTML('beforeend', `<p>${tarefa.value}</p>`)
+        listaTarefas.style.backgroundColor = 'white'
+    }
+}
